33dfa7d17b61c80cdf70118c5f3c4227337bc3a5
zhoukang <zhoukang@yuxun.com.cn>
2018-01-11 16:43:36 +0800

    sdk 更新

--- app/src/main/java/com/android11/wallpager/home/HomeActivity.java
+++ app/src/main/java/com/android11/wallpager/home/HomeActivity.java
@@ -53,7 +53,7 @@ public class HomeActivity extends BaseActivity implements NavigationView.OnNavig
 
     @Override
     public void onBackPressed() {
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        DrawerLayout drawer = findViewById(R.id.drawer_layout);
         if (drawer.isDrawerOpen(GravityCompat.START)) {
             drawer.closeDrawer(GravityCompat.START);
         } else {
@@ -67,7 +67,7 @@ public class HomeActivity extends BaseActivity implements NavigationView.OnNavig
     }
 
     private void initView() {
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        Toolbar toolbar = findViewById(R.id.toolbar);
 
         toolbar.setTitle("摄影&壁纸");
         setSupportActionBar(toolbar);
